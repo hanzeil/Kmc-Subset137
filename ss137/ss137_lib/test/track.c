e9d8563f5dd4973d5ef63db203521db5b1114f3b
Alessandro Pesoli <alessandro.pesoli@neat.it>
2016-03-02 19:02:19 +0100

    Added functions to initialize connection at application level. Added functions to decode message from net to host format.

---
+++ ss137/ss137_lib/test/track.c
@@ -0,0 +1,26 @@
+#include <stdio.h>     /* for fopen, snprintf, etc... */
+#include <string.h>    /* for memmove, memcmp, memset */
+#include <arpa/inet.h> /* for htons, etc.. */
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "common.h"
+#include "net_utils.h"
+#include "ss137_lib.h"
+
+int main(int argc, char *argv[])
+{
+	int32_t sock = 0;
+	session_t session;
+
+	memset(&session, 0, sizeof(session_t));
+		
+	initServerConnection(&session.ssl_des, &sock, atoi(argv[1]));
+
+	initAppSession(0x11223344, &session);
+	
+	/* endAppSession(&session); */
+
+	return(0);
+}
