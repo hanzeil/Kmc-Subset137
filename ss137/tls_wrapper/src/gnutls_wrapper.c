500d1229e63e815b2f3d268a993ced6259ec36bc
Alessandro Pesoli <alessandro.pesoli@neat.it>
2016-03-15 10:59:54 +0100

    Added gnutls wrapper

--- ss137/tls_wrapper/src/gnutls_wrapper.c
+++ ss137/tls_wrapper/src/gnutls_wrapper.c
@@ -1,11 +1,11 @@
 /**************************************************************************//**
  *
- * TLS wrapper for OpenSSL
+ * TLS wrapper for GnuTLS
  *
  * This file wraps the SS137-TLS-needs in an uniform API. This implementation
- * uses OpenSSL as low-level library
+ * uses GnuTLS as low-level library
  *
- * @file: ss137/tls_wrapper/src/tls_wrapper.c
+ * @file: ss137/tls_wrapper/src/gnutls_wrapper.c
  * $Author: $
  * $Revision: $
  * $Date: $
@@ -27,19 +27,15 @@
 #include <unistd.h>
 #include <poll.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <openssl/crypto.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-#include <openssl/bio.h>
-#include <openssl/err.h>
-#include <openssl/pem.h>
-#include <openssl/x509.h>
-#include <openssl/x509_vfy.h>
+#include <gnutls/gnutls.h>
+#include <gnutls/x509.h>
 
 #include "utils.h" 
 #include "tls_wrapper.h"
-
 
 /*****************************************************************************
  * DEFINES
@@ -61,31 +57,28 @@ typedef struct
 {
 	bool_t  in_use;
 	int32_t socket;
-	SSL    *ssl_ptr;
+	gnutls_session_t session;
 } tls_descriptor_t;
 
 /*****************************************************************************
  * VARIABLES
  *****************************************************************************/
 
-const char allowed_ciphers[] = "ECDHE-RSA-AES256-GCM-SHA384";  	/** List of ciphers. String is colon-separated i.e. "CIPHERA:CIPHERB:CIPHERC" */
-static SSL_CTX *ctx = NULL;
+const char priority_string[] = "NONE:+VERS-TLS1.2:+MAC-ALL:+ECDHE-RSA:+AES-256-GCM:+SIGN-RSA-SHA384:+COMP-NULL:+CTYPE-ALL:+CURVE-ALL";
 
 static tls_descriptor_t tls_descriptors[MAX_TLS_DES];
 
 static int32_t listen_sock = -1;
 
+static gnutls_certificate_credentials_t x509_cred;
+
 /*****************************************************************************
  * FUNCTION PROTOTYPES
  *****************************************************************************/
 
-static tls_error_code_t getPeerCertificate(SSL* ssl);
-
-static tls_error_code_t initTLS(const char* const ca_cert, const char *const key, const char* const cert);
-
 static tls_error_code_t findTLSDes(uint32_t * const tls_id);
 
-static tls_error_code_t verifyLocalCertificate(const char* const ca_cert, const char* const cert);
+static tls_error_code_t initTLS(const char* const ca_cert, const char *const key, const char* const cert);
 
 /*****************************************************************************
  * LOCAL FUNCTION DECLARATIONS
@@ -96,7 +89,7 @@ static tls_error_code_t verifyLocalCertificate(const char* const ca_cert, const 
  */
 static tls_error_code_t findTLSDes
 (
-	uint32_t * const tls_id /**< [in] SSL context */
+	uint32_t * const tls_id /**< [in] TLS context */
 	)
 {
 	uint32_t i = 0U;
@@ -124,110 +117,10 @@ static tls_error_code_t findTLSDes
 	return(TLS_SUCCESS);
 }
 
-static tls_error_code_t verifyLocalCertificate(const char* const ca_cert,
-											   const char* const cert)
-{
-	
-  X509  *cert_str = NULL;
-  X509_STORE  *store = NULL;
-  X509_STORE_CTX *vrfy_ctx = NULL;
-  BIO   *certbio = NULL;
-  int ret;
-
-  certbio = BIO_new(BIO_s_file());
-  OpenSSL_add_all_algorithms();
-  ERR_load_BIO_strings();
-  
-  if ((store = X509_STORE_new()) == 0)
-  {
-	  log_print("Error creating X509 store object\n");
-	  return(TLS_ERROR);
-  }
-
-  vrfy_ctx = X509_STORE_CTX_new();
-
-  /* load the certificate and ca certificate in memory */
-  ret = BIO_read_filename(certbio, cert);
-  if ((cert_str = PEM_read_bio_X509(certbio, NULL, 0, NULL)) == 0)
-  {
-    log_print("Error loading cert into memory\n");
-    return(TLS_ERROR);
-  }
-
-  if (X509_STORE_load_locations(store, ca_cert, NULL) != 1)
-  {
-    log_print("Error loading CA cert or chain file\n");
-	return(TLS_ERROR);
-  }
-
-   /* Initialize the ctx structure for a verification operation*/
-  X509_STORE_CTX_init(vrfy_ctx, store, cert_str, NULL);
-
-/* Check the complete cert chain. */
-  ret = X509_verify_cert(vrfy_ctx);
-  if(ret == 0)
-  {
-	  err_print("Local certificate verification failed\n");
-	  err_print("Result: %s\n", X509_verify_cert_error_string(vrfy_ctx->error));
-	  return(TLS_ERROR);
-  }
-  else
-  {
-	  log_print("Local certificate is valid\n");
-  }
-
-  /* free used structure */
-  X509_STORE_CTX_free(vrfy_ctx);
-  X509_STORE_free(store);
-  X509_free(cert_str);
-
-  return(TLS_SUCCESS);
-}
-
-/**
- * Some useful Doxygen comment
- */
-static tls_error_code_t getPeerCertificate
-(
-	SSL* ssl /**< [in] SSL context */
-	)
-{
-	char *str;
-	X509 *peer_cert;
-
-	ASSERT(ssl != NULL, E_NULL_POINTER);
-	
-	peer_cert = SSL_get_peer_certificate (ssl);    
-	
-	if (peer_cert != NULL)
-	{
-		log_print("Peer certificate\n");
-		
-		str = X509_NAME_oneline(X509_get_subject_name(peer_cert),0,0);
-		log_print("\t subject: %s\n", str);
-		free (str);
-		
-		str = X509_NAME_oneline(X509_get_issuer_name(peer_cert),0,0);
-		log_print("\t issuer: %s\n", str);
-		free(str);
-		
-		X509_free (peer_cert);
-	}
-	else
-	{
-		err_print("The SSL peer does not have certificate.\n");
-		return(TLS_ERROR);
-	}
-	
-	return(TLS_SUCCESS);
-}
-
 static tls_error_code_t initTLS(const char* const ca_cert,
 								const char *const key,
 								const char* const cert)
 {
-	const SSL_METHOD * meth = NULL;
-
 	ASSERT(ca_cert != NULL, E_NULL_POINTER);
 	ASSERT(key != NULL, E_NULL_POINTER);
 	ASSERT(cert != NULL, E_NULL_POINTER);
@@ -236,89 +129,83 @@ static tls_error_code_t initTLS(const char* const ca_cert,
 	log_print("RSA Key:  %s\n", key);
 	log_print("RSA Cert: %s\n", cert);
 	
-	/* Load encryption & hashing algorithms for the SSL program */
-	SSL_library_init();
- 
-    /* Load the error strings */
-	SSL_load_error_strings();
- 
-	/* Create an SSL_METHOD structure for TLSv1.2 */
-	meth = TLSv1_2_method();
-	if( meth == NULL )
-	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
-	
-	/* Create an SSL_CTX structure */
-	ctx = SSL_CTX_new(meth);
-	if( meth == NULL )
-	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
- 
-	/* Load the certificate into the SSL_CTX structure */
-	if(!SSL_CTX_use_certificate_file(ctx, cert, SSL_FILETYPE_PEM))
-	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
-	
-	/* Load the private-key corresponding to the certificate */
-	if(!SSL_CTX_use_PrivateKey_file(ctx, key, SSL_FILETYPE_PEM))
-	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
-	
-	/* Check if the certificate and private-key matches */
-	if (!SSL_CTX_check_private_key(ctx))
-	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
- 
-	/* Load the RSA CA certificate into the SSL_CTX structure */
-	/* This will allow  to verify the peer's  */
-	if(!SSL_CTX_load_verify_locations(ctx, ca_cert, NULL))
-	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
- 
-	/* Set flag in context to require peer certificate */
-	/* verification */
-	SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
+	/* for backwards compatibility with gnutls < 3.3.0 */
+	gnutls_global_init();
 
-	/* set the verify depth*/
-	SSL_CTX_set_verify_depth(ctx,VERIFY_DEPTH);
+	 /* X509 stuff */
+	gnutls_certificate_allocate_credentials(&x509_cred);
 
-	/* verify local certificate */
-	if(verifyLocalCertificate(ca_cert, cert) != TLS_SUCCESS)
-	{
-		return(TLS_ERROR);
-		
-	}
+	/* sets the trusted CA file*/
+	gnutls_certificate_set_x509_trust_file(x509_cred, ca_cert, GNUTLS_X509_FMT_PEM);
+
+	/* Set local certificate and key*/
+	gnutls_certificate_set_x509_key_file (x509_cred, cert, key, GNUTLS_X509_FMT_PEM);
+
 	return(TLS_SUCCESS);
 
+}
+
+
+static tls_error_code_t verifyPeer(gnutls_session_t session)
+{
+	unsigned int status;
+
+	if (gnutls_certificate_verify_peers2 (session, &status) < 0)
+    {
+		err_print ("Error on peer certificate\n");
+		return(TLS_ERROR);
+    }
+
+	if (status & GNUTLS_CERT_SIGNER_NOT_FOUND)
+	{
+		err_print ("The certificate hasn't got a known issuer.\n");
+		return(TLS_ERROR);
+	}
+	
+	if (status & GNUTLS_CERT_REVOKED)
+	{
+		err_print ("The certificate has been revoked.\n");
+		return(TLS_ERROR);
+	}
+	
+	if (status & GNUTLS_CERT_EXPIRED)
+	{
+		err_print ("The certificate has expired\n");
+		return(TLS_ERROR);
+	}
+	
+	if (status & GNUTLS_CERT_NOT_ACTIVATED)
+	{
+		err_print ("The certificate is not yet activated\n");
+		return(TLS_ERROR);
+	}
+	
+	if (status & GNUTLS_CERT_INVALID)
+    {
+		err_print ("The certificate is not trusted.\n");
+		return GNUTLS_E_CERTIFICATE_ERROR;
+    }
+
+	log_print("The peer's certificate is valid\n");
+	return(TLS_SUCCESS);
 }
 
 /*****************************************************************************
  * PUBLIC FUNCTION DECLARATIONS
  *****************************************************************************/
 
+/* client */
 tls_error_code_t initClientTLS(uint32_t* const tls_id,
 							   const char* const ca_cert,
 							   const char *const key,
 							   const char* const cert)
 {
 	int32_t tmp_sock = -1;
+	gnutls_priority_t priority_cache;
 
 	ASSERT(ca_cert != NULL, E_NULL_POINTER);
 	ASSERT(key != NULL, E_NULL_POINTER);
 	ASSERT(cert != NULL, E_NULL_POINTER);
-
 	ASSERT(tls_id != NULL, E_NULL_POINTER);
 
 	if(initTLS(ca_cert, key, cert) != TLS_SUCCESS)
@@ -326,14 +213,37 @@ tls_error_code_t initClientTLS(uint32_t* const tls_id,
 		return(TLS_ERROR);
 	}
 
-	if(findTLSDes(tls_id) != TLS_SUCCESS)
+	if(gnutls_init(&tls_descriptors[*tls_id].session, GNUTLS_CLIENT) != GNUTLS_E_SUCCESS)
 	{
 		return(TLS_ERROR);
 	}
 	
+	if(findTLSDes(tls_id) != TLS_SUCCESS)
+	{
+		return(TLS_ERROR);
+	}
+
+	if(gnutls_priority_init(&priority_cache, priority_string, NULL) != GNUTLS_E_SUCCESS)
+	{
+		return(TLS_ERROR);
+	}
+	
+	if(gnutls_priority_set(tls_descriptors[*tls_id].session, priority_cache) != GNUTLS_E_SUCCESS)
+	{
+		return(TLS_ERROR);
+	}
+	
+	if(gnutls_credentials_set(tls_descriptors[*tls_id].session, GNUTLS_CRD_CERTIFICATE, x509_cred) != GNUTLS_E_SUCCESS)
+	{
+		return(TLS_ERROR);
+	}
+	
+	gnutls_session_set_verify_cert(tls_descriptors[*tls_id].session, NULL, 0);
+	
 	tmp_sock = socket (PF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if(tmp_sock == -1)
 	{
+		err_print("Error opening socket\n");
 		return(TLS_ERROR);
 	}
 
@@ -347,59 +257,66 @@ tls_error_code_t connectTLS(const uint32_t tls_id,
 							const uint16_t server_port)
 {
 	struct sockaddr_in server_addr;
-	SSL* ssl = NULL;
-	
+	int32_t ret;
+	uint8_t status = 1;
+	int32_t type;
+	gnutls_datum_t out;
+		
 	ASSERT(tls_id < MAX_TLS_DES, E_INVALID_PARAM);
 	ASSERT(server_ip != NULL, E_NULL_POINTER);
 
 	memset (&server_addr, '\0', sizeof(server_addr));
 	server_addr.sin_family      = AF_INET;
  	server_addr.sin_port        = htons(server_port);     /* Server Port number */
-	server_addr.sin_addr.s_addr = inet_addr(server_ip);        /* Server IP */
+	server_addr.sin_addr.s_addr = inet_addr(server_ip);   /* Server IP */
 
-	/* Establish a TCP/IP connection to the SSL client */
+	/* Establish a TCP/IP connection to the TLS client */
 	if(connect(tls_descriptors[tls_id].socket, (struct sockaddr*) &server_addr, sizeof(server_addr)) == -1)
 	{
 		err_print("Error on connect to server (%s:%d)\n", server_ip, server_port);
 		return(TLS_ERROR);
 	}
 
-	ssl = SSL_new (ctx);
-	if(ssl == NULL)
+	gnutls_transport_set_int(tls_descriptors[tls_id].session, tls_descriptors[tls_id].socket);
+
+	gnutls_handshake_set_timeout(tls_descriptors[tls_id].session, GNUTLS_DEFAULT_HANDSHAKE_TIMEOUT);
+
+	/* Perform the TLS handshake     */
+	do
 	{
-		ERR_print_errors_fp(stderr);
+		ret = gnutls_handshake(tls_descriptors[tls_id].session);
+	}
+	while (ret < 0 && gnutls_error_is_fatal(ret) == 0);
+	if (ret < 0)
+	{
+		err_print("*** TLS Handshake failed\n");
+		gnutls_perror(ret);
+		return(TLS_ERROR);
+	}
+	else
+	{
+		char *desc = NULL;
+		desc = gnutls_session_get_desc(tls_descriptors[tls_id].session);
+		log_print("Session info: %s\n", desc);
+		gnutls_free(desc);
+	}
+	
+	verifyPeer(tls_descriptors[tls_id].session);
+	
+	type = gnutls_certificate_type_get(tls_descriptors[tls_id].session);
+
+	status = gnutls_session_get_verify_cert_status(tls_descriptors[tls_id].session);
+
+	ret = gnutls_certificate_verification_status_print(status, type, &out, 0);
+	if (ret < 0)
+	{
+		err_print("Peer certificate verification failed\n");
 		return(TLS_ERROR);
 	}
 
-	if(!SSL_set_cipher_list(ssl, allowed_ciphers))
-	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
-
-	/* Assign the socket into the SSL structure (SSL and socket without BIO) */
-	if(!SSL_set_fd(ssl, tls_descriptors[tls_id].socket))
-	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
-
-	/* Perform SSL Handshake on the SSL client */
-	if(!SSL_connect(ssl))
-	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
-
-	log_print("SSL connection using %s\n", SSL_get_cipher_list(ssl, 0));
-
-	tls_descriptors[tls_id].ssl_ptr = ssl;
-
-	if(getPeerCertificate(ssl) != TLS_SUCCESS)
-	{
-		return(TLS_ERROR);
-	}
-
+	log_print("%s\n", out.data);
+	gnutls_free(out.data);
+	
 	return(TLS_SUCCESS);
 }
 
@@ -410,21 +327,21 @@ tls_error_code_t initServerTLS(const uint16_t l_port,
 							   const char* const cert)
 {
 	struct sockaddr_in sa_serv;
-	EC_KEY  *ecdh;
-
+	
 	ASSERT(ca_cert != NULL, E_NULL_POINTER);
 	ASSERT(key != NULL, E_NULL_POINTER);
 	ASSERT(cert != NULL, E_NULL_POINTER);
 
 	if(initTLS(ca_cert, key, cert) != TLS_SUCCESS)
 	{
+		err_print("Error during initTLS()\n");
 		return(TLS_ERROR);
 	}
 	
 	listen_sock = socket (PF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if(listen_sock == -1)
 	{
-		err_print("Error opening socket\n");
+		err_print("Error opening listen socket\n");
 		return(TLS_ERROR);
 	}
 
@@ -435,30 +352,15 @@ tls_error_code_t initServerTLS(const uint16_t l_port,
 
 	if(bind(listen_sock, (struct sockaddr*)&sa_serv, sizeof(sa_serv)) == -1)
 	{
-		err_print("Error on binding port %d\n", l_port);
+		err_print("Error on binding() port %d\n", l_port);
 		return(TLS_ERROR);
 	}
 
 	if(listen(listen_sock, 5) == -1)
 	{
-		err_print("Error on listen call\n");
+		err_print("Error on listen() call\n");
 		return(TLS_ERROR);
 	}
-
-
-	/* set the elliptic curve to be used during key exchange */
-	ecdh = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1);
-	if (ecdh == NULL)
-	  {
-	    err_print("Failed to get EC curve");
-	    return(TLS_ERROR);
-	  }
-	else
-	  {
-
-	    SSL_CTX_set_tmp_ecdh(ctx, ecdh);
-	    EC_KEY_free(ecdh);
-	  }
 
 	return(TLS_SUCCESS);
 }
@@ -469,8 +371,9 @@ tls_error_code_t acceptTLS(uint32_t* const tls_id,
 	struct sockaddr_in sa_cli;
 	int32_t  client_len = 0;
 	int32_t client_sock = -1;
-	SSL *ssl = NULL;
-
+	int32_t ret = -1;
+	gnutls_priority_t priority_cache;
+		
 	ASSERT(tls_id != NULL, E_NULL_POINTER);
 	ASSERT(client_ip != NULL, E_NULL_POINTER);
 
@@ -479,10 +382,20 @@ tls_error_code_t acceptTLS(uint32_t* const tls_id,
 		return(TLS_ERROR);
 	}
 
+	gnutls_init(&tls_descriptors[*tls_id].session, GNUTLS_SERVER);
+	
+	gnutls_priority_init(&priority_cache, priority_string, NULL);
+	
+	gnutls_priority_set(tls_descriptors[*tls_id].session, priority_cache);
+	
+	gnutls_credentials_set(tls_descriptors[*tls_id].session, GNUTLS_CRD_CERTIFICATE,  x509_cred);
+	
+	/* require the client certificate, the server will return an error if the peer does not provide a certificate */
+	gnutls_certificate_server_set_request(tls_descriptors[*tls_id].session, GNUTLS_CERT_REQUIRE);
+	
 	client_len = sizeof(sa_cli);
 
 	/* Wait for an incoming TCP connection. */
-	/* Socket for a TCP/IP connection is created */
 	client_sock = accept(listen_sock, (struct sockaddr*)&sa_cli, (socklen_t *)&client_len);
 	if(client_sock == -1)
 	{
@@ -495,61 +408,39 @@ tls_error_code_t acceptTLS(uint32_t* const tls_id,
 			(sa_cli.sin_addr.s_addr >> 16 & 0xFF),
 			(sa_cli.sin_addr.s_addr >> 24 & 0xFF));
 	
-	ssl = SSL_new(ctx);
-	if(ssl == NULL)
-	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
-
-	if(!SSL_set_cipher_list(ssl, allowed_ciphers))
-	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
-		
-	/* Assign the socket into the SSL structure (SSL and socket without BIO) */
-	if(!SSL_set_fd(ssl, client_sock))
-	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
-
-	/* Perform SSL Handshake on the SSL server */
-	if(!SSL_accept(ssl))
- 	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
-
-	if(getPeerCertificate(ssl) != TLS_SUCCESS)
-	{
-		return(TLS_ERROR);
-	}
-
-	tls_descriptors[*tls_id].ssl_ptr = ssl;
 	tls_descriptors[*tls_id].socket = client_sock;
 
+	gnutls_transport_set_int(tls_descriptors[*tls_id].session, tls_descriptors[*tls_id].socket);
+	
+	do {
+		ret = gnutls_handshake(tls_descriptors[*tls_id].session);
+	}
+	while (ret < 0 && gnutls_error_is_fatal(ret) == 0);
+	
+	if (ret < 0)
+	{
+		err_print("TLS Handshake failed (%s)\n\n", gnutls_strerror(ret));
+		return(TLS_ERROR);
+	}
+
+	verifyPeer(tls_descriptors[*tls_id].session);
+	
+	log_print("TLS Handshake completed\n");
+	
 	return(TLS_SUCCESS);
 }
 
 tls_error_code_t closeTLS(const uint32_t tls_id)
 {
 	ASSERT(tls_id < MAX_TLS_DES, E_INVALID_PARAM);
-	
-	if(!SSL_shutdown(tls_descriptors[tls_id].ssl_ptr))
- 	{
-		ERR_print_errors_fp(stderr);
-		return(TLS_ERROR);
-	}
 
-	/* Free the SSL structure
-	   and close the socket*/
-	SSL_free(tls_descriptors[tls_id].ssl_ptr);
+	gnutls_bye(tls_descriptors[tls_id].session, GNUTLS_SHUT_RDWR);
+
+	gnutls_deinit(tls_descriptors[tls_id].session);
+
 	close(tls_descriptors[tls_id].socket);
 
 	tls_descriptors[tls_id].socket = -1;
-	tls_descriptors[tls_id].ssl_ptr = NULL;
 	tls_descriptors[tls_id].in_use = FALSE;
 
 	return(TLS_SUCCESS);
@@ -564,11 +455,10 @@ tls_error_code_t sendTLS(uint32_t* const bytes_sent,
 	ASSERT(bytes_sent != NULL, E_NULL_POINTER);
 	ASSERT(buf != NULL, E_NULL_POINTER);
 	
-	*bytes_sent = SSL_write(tls_descriptors[tls_id].ssl_ptr, buf, buf_len);
+	*bytes_sent = gnutls_record_send(tls_descriptors[tls_id].session, buf, buf_len);
 
 	if(*bytes_sent <= 0)
 	{
-		ERR_print_errors_fp(stderr);
 		return(TLS_ERROR);
 	}
 
@@ -613,15 +503,21 @@ tls_error_code_t receiveTLS(uint32_t* const bytes_received,
 		}
 		else if (handles[0].revents & POLLIN)
 		{
-			*bytes_received = SSL_read(tls_descriptors[tls_id].ssl_ptr, buf, buf_len);
-			
-			if(*bytes_received <= 0)
+			*bytes_received = gnutls_record_recv(tls_descriptors[tls_id].session, buf, buf_len);
+
+			if (*bytes_received == 0)
 			{
-				if(SSL_get_shutdown(tls_descriptors[tls_id].ssl_ptr) == SSL_RECEIVED_SHUTDOWN)
-				{
-					warning_print("TLS shutdown received from the other peer\n");
-					return(TLS_ERROR);
-				}
+				log_print("Peer has closed the TLS connection\n");
+				return(TLS_ERROR);
+			}
+			else if (*bytes_received < 0 && gnutls_error_is_fatal(*bytes_received) == 0)
+			{
+				log_print("%s\n", gnutls_strerror(*bytes_received));
+			}
+			else if (*bytes_received < 0)
+			{
+				err_print("%s\n", gnutls_strerror(*bytes_received));
+				return(TLS_ERROR);
 			}
 		}
 	}
@@ -631,8 +527,10 @@ tls_error_code_t receiveTLS(uint32_t* const bytes_received,
 
 tls_error_code_t exitTLS(void)
 {
-	/* Free the SSL_CTX structure */
-	SSL_CTX_free(ctx);
+
+	gnutls_certificate_free_credentials(x509_cred);
+	
+	gnutls_global_deinit();
 
 	/* only the server side has a
 	   listen_sock to close */
